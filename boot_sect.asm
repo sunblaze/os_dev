100fd72771db2fb691342663cf3ec15632d7c0a6
James Reid-Smith <james.smith@cenx.com>
2013-04-20 19:03:38 -0400

    Finished four atempts at printing X, had to fix absolute value because of differences in assembly compilation

--- boot_sect.asm
+++ boot_sect.asm
@@ -4,18 +4,28 @@
 
 mov ah, 0x0e          ;int 10/ah = 0eh -> scrolling teletype BIOS routine
 
-mov al, 'H'
-int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'o'
-int 0x10
+; First attempt
+mov al, the_secret
+int 0x10              ; Does this print an X?
 
-jmp $
+; Second attempt
+mov al, [the_secret]
+int 0x10              ; Does this print an X?
+
+; Third attempt
+mov bx, the_secret
+add bx, 0x7c00
+mov al, [bx]
+int 0x10              ; Does this print an X?
+
+; Fourth attempt
+mov al, [0x7c1d]
+int 0x10              ; Does this print an X?
+
+jmp $                 ; Jump forever.
+
+the_secret:
+  db "X"
 
 ;
 ; Padding and magic BIOS number.
